7898c0715618817405a6102fa323ff5d95a697a3
Gerardo Lopez Falcon <gelopfalcon@gmail.com>
2022-12-07 14:31:35 -0600

    Create HelloWorld.sol

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+contract HelloWorld {
+
+    /// @return "Hello HelloWorld"
+    function hello() external pure returns (string memory) {
+        return "Hello World"
+    }
+
+}
